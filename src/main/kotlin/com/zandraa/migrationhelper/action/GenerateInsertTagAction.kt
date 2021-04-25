2404123853d54108a3b389f15a02116077d10aaf
Zandraa <zandraa.perjesi@gmail.com>
2021-04-25 22:19:59 +0200

    Handle Multiple inserts and handle @Column annotations

--- src/main/kotlin/com/zandraa/migrationhelper/action/GenerateInsertTagAction.kt
+++ src/main/kotlin/com/zandraa/migrationhelper/action/GenerateInsertTagAction.kt
@@ -6,16 +6,17 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiField
 import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiType
 import com.intellij.psi.impl.source.PsiJavaFileImpl
 import com.zandraa.migrationhelper.dialog.InsertDialog
-import com.zandraa.migrationhelper.dialog.TestDialog
 import com.zandraa.migrationhelper.util.COLUMN_ANNOTATION
 import com.zandraa.migrationhelper.util.XMLUtil
+import com.zandraa.migrationhelper.util.camelToSnake
 import com.zandraa.migrationhelper.util.insertTextAtCaret
+import java.lang.Exception
 
-class GenerateInsertTagAction(): AnAction() {
+class GenerateInsertTagAction() : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project
@@ -32,26 +33,31 @@ class GenerateInsertTagAction(): AnAction() {
             val dialog = InsertDialog(project)
             dialog.showAndGet()
 
-            val times: String = dialog.getTimes()
+            val times: Int = try {
+                dialog.getTimes().toInt()
+            } catch (e: Exception) {
+                1
+            }
 
             val chosenFilePsi = PsiManager.getInstance(project).findFile(chosenFile)
             val classes = (chosenFilePsi as PsiJavaFileImpl).classes
 
-            val entityFields = mutableListOf<PsiField>()
+            val entityFields = mutableListOf<Pair<String, PsiType>>() //name,type
 
             for (clazz in classes) {
-                entityFields.addAll(clazz.allFields)
-                for (field in clazz.allFields) {
-                    var fieldName = field.name
-                    for (annotation in field.annotations) {
+                val fieldList = clazz.allFields.map {
+                    var fieldName = it.name.camelToSnake()
+                    for (annotation in it.annotations) {
                         if (COLUMN_ANNOTATION.matches((annotation as PsiElement).text)) {
-                            println("MATCH " + (annotation as PsiElement).text)
+                            val (columnName) = COLUMN_ANNOTATION.find((annotation as PsiElement).text)!!.destructured
+                            fieldName = columnName
                         }
                     }
+                    Pair(fieldName, it.type)
                 }
-                //(clazz.allFields.get(0).annotations[1] as PsiElement).text
+                entityFields.addAll(fieldList)
             }
-            val addInsert = XMLUtil.addInsert(entityFields, classes[0].name)
+            val addInsert = XMLUtil.addInserts(entityFields, classes[0].name, times)
             if (editor != null) {
                 insertTextAtCaret(
                     editor.caretModel.primaryCaret,
--- src/main/kotlin/com/zandraa/migrationhelper/util/XMLUtil.kt
+++ src/main/kotlin/com/zandraa/migrationhelper/util/XMLUtil.kt
@@ -1,26 +1,32 @@
 package com.zandraa.migrationhelper.util
 
 import com.intellij.openapi.util.NlsSafe
-import com.intellij.psi.PsiField
 import com.intellij.psi.PsiType
 import org.jetbrains.annotations.Nullable
 
 class XMLUtil {
     companion object {
-        fun addInsert(entityFields: MutableList<PsiField>, name: @Nullable @NlsSafe String?): String {
+        fun addInserts(entityFields: MutableList<Pair<String, PsiType>>, name: @Nullable @NlsSafe String?, times: Int): String {
+            val insertTag = generateInsert(entityFields, name)
+            var insertList = MutableList(times) {insertTag}
+            return insertList.joinToString(separator = "\n") { it }
+                .trimStart()
+        }
+
+        private fun generateInsert(entityFields: MutableList<Pair<String, PsiType>>, name: @Nullable @NlsSafe String?): String {
             var generateColumnTag = entityFields.map { generateColumnTag(it) }
                 .joinToString(separator = "\n") { it }
-            return """<insert tableName="${name?.camelToSnake()}">
+            return """        <insert tableName="${name?.camelToSnake()}">
 $generateColumnTag
         </insert>"""
         }
 
-        private fun generateColumnTag(field: PsiField): String {
+        private fun generateColumnTag(field: Pair<String, PsiType>): String {
 
-            return when (field.type) {
-                PsiType.BOOLEAN -> """            <column name="${field.name.camelToSnake()}" valueBoolean=""/>"""
+            return when (field.second) {
+                PsiType.BOOLEAN -> """            <column name="${field.first}" valueBoolean=""/>"""
                 //todo date
-                else -> """            <column name="${field.name.camelToSnake()}" value=""/>"""
+                else -> """            <column name="${field.first}" value=""/>"""
             }
         }
     }
